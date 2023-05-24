0dafd2b730998a599d1d316c714f9f273577e44b
David-McCaig <davidmccaig1@gmail.com>
2023-05-25 09:55:28 +1200

    Update seed data

--- seeds/c_chat.js
+++ seeds/c_chat.js
@@ -8,27 +8,27 @@
     await knex('chat').insert([
       {
         id: '0a09308f-e60c-4e76-a4d9-28b389d916c1',
-        product_id: '3b16f79c-4efd-43ee-8ec4-a65439d5fe5e',
-        message: 'Hello parts bin test',
+        product_id: '7df72176-7064-4c9b-adc7-c406e3da0b47',
+        message: 'Hi, is this still available',
         room: 'a0e58318-1dac-492f-8b96-c880929fa416',
         user_name: 'Frank',
         email: 'frank@gmail.com',
       },
       {
         id: '25131af4-11dc-4221-a74e-50e5a9531bd0',
-        product_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
-        message: 'Hello parts bin test dave',
+        product_id: 'ce9c3ecf-c766-4421-b70e-3e79a485bfa3',
+        message: 'Hi, is this still available',
         room: 'd83507e9-f103-4a1e-9231-44e97b3c297d',
-        user_name: 'dave',
-        email: 'dave@gmail.com',
+        user_name: 'Jason Data',
+        email: 'jason@gmail.com',
       },
       {
         id: '97cb082b-621a-494c-bae6-34f8723433db',
-        product_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
-        message: 'Hello parts bin test dave',
-        room: 'd83507e9-f103-4a1e-9231-44e97b3c297d',
-        user_name: 'frank',
-        email: 'frank@gmail.com',
+        product_id: '48aa03d1-8fae-4979-87f8-31798a29d9f5',
+        message: 'Hi, is this still available',
+        room: 'caed0924-c9c1-4bed-bec3-458c708668f1',
+        user_name: 'Brock Kelly',
+        email: 'brock@gmail.com',
       },
     ]);
   };
